b85579adb77fc0060f7c63a4c50266d961bb0bbd
Wei Xin <czdxwx@gmail.com>
2023-06-06 04:59:50 +0800

    Revert "fucking change"
    This reverts commit f9511d8d658ee1f3836dbf7373d0d643f0d6c6cc.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginViewVue from '../views/LoginView.vue'
-import HomePageVue from '../views/HomePage.vue'
-import firstViewVue from '../views/tabViews/firstView.vue'
-import secondViewVue from '../views/tabViews/firstView.vue'
-import thirdViewVue from '../views/tabViews/firstView.vue'
-import forthViewVue from '../views/tabViews/firstView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'Login',
-            component: LoginViewVue
-        },
-        {
-            path: '/Home',
-            name: 'Home',
-            component: () => import('../views/HomePage.vue'),
-            children: [
-                { path: '/nav1', component: firstViewVue },
-                { path: '/nav2', component: secondViewVue },
-                { path: '/nav3', component: thirdViewVue },
-                { path: '/nav4', component: forthViewVue },
-            ],
-            meta: {
-                IsLogin: true,
-            }
-        }
-    ]
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
 })
 
 export default router
